43965fac46d49cdcfba9b13f4cfbd833ae76cf13
safiullah <safiullah123fix@gmail.com>
2023-07-24 14:51:19 +0500

    fifth commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import ContactUs from "./pages/contactUs/ContactUs";
 import Register from "./pages/login/Register";
-// import ShoppingCart from "./pages/shoppingCart/ShoppingCart";
-// import CheckOut from "./pages/check/CheckOut";
+import ShoppingCart from "./pages/shoppingCart/ShoppingCart";
+import CheckOut from "./pages/check/CheckOut";
 import UserAccount from "./pages/account/UserAccount";
 import AboutUs from './pages/about/AboutUs';
-// import Header from "./Components/common/Header";
-// import Footer from "./Components/common/Footer";
+import Header from "./Components/common/Header";
+import Footer from "./Components/common/Footer";
 import Afaq from "./pages/FAQ/Afaq";
-// import Review from "./Components/common/Review";
+import Review from "./Components/common/Review";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -30,20 +30,20 @@ function ScrollToTop() {
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header/> */}
+      <Header/>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/Product" element={<Product/>}/>
         <Route path="/ContactUs" element={<ContactUs/>}/>
         <Route path="/Register" element={<Register/>}/> 
-        {/* <Route path="/ShoppingCart" element={<ShoppingCart/>}/> */}
-        {/* <Route path="/Checkout" element={<CheckOut/>}/> */}
+        <Route path="/ShoppingCart" element={<ShoppingCart/>}/>
+        <Route path="/Checkout" element={<CheckOut/>}/>
         <Route path="/UserAccount" element={<UserAccount/>}/>
         <Route path="/AboutUs" element={<AboutUs/>}/>
         <Route path="/FAQ" element={<Afaq/>}/>
       </Routes> 
-      {/* <Footer/> */}
+       <Footer/>
     </BrowserRouter>
   );
 }
